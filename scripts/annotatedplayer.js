bd10ae16230a14dee14a3d6c137cc6dd27392c1b
Arthur McBain <mcbain.asm@gmail.com>
2014-11-15 01:19:27 -0800

    Added AnnotatedPlayer stub.

---
+++ scripts/annotatedplayer.js
@@ -0,0 +1,22 @@
+// NOTEs: turn player background a nice shade of red or something if
+// the browser says it can't play the given mime or it fails even if
+// returns "maybe" for canPlayType.
+function AnnotatedPlayer (url, mime, info)
+{
+    var player = new Audio();
+
+    this.addEventListener = function (event, callback)
+    {
+        player.addEventListener(event, callback);
+    };
+
+    this.play = function ()
+    {
+        player.play();
+    };
+
+    this.pause = function ()
+    {
+        player.pause();
+    };
+}
